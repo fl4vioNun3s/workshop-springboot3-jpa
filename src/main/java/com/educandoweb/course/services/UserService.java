c2c0718100c723ffd8863a1e97874c54a973611f
Flavio Nunes <flavionbueri@gmail.com>
2023-01-18 01:32:24 -0300

    Camada de serviço e registro de componentes

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		//Uma classe que existe desde o Java 8.
+		Optional<User> obj = repository.findById(id);
+		
+		//O get vai retornar o tipo do Optional, nesse caso, o User.
+		return obj.get();
+	}
+}
