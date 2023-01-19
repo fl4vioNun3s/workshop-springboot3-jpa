d197d74fca2a95760d1f7b2432c9ae27747d1a38
Flavio Nunes <flavionbueri@gmail.com>
2023-01-19 01:24:53 -0300

    Entidade Category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		//Uma classe que existe desde o Java 8.
+		Optional<Category> obj = repository.findById(id);
+		
+		//O get vai retornar o tipo do Optional, nesse caso, o Category.
+		return obj.get();
+	}
+}
